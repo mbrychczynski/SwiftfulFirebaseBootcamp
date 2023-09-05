7cf00c3b6a881a98245c129d4634fd22ac2774aa
Mateusz Brychczynski <mat.brych@gmail.com>
2023-09-05 17:06:05 +0100

    Configure the SDK

--- SwiftfulFirebaseBootcamp/SwiftfulFirebaseBootcampApp.swift
+++ SwiftfulFirebaseBootcamp/SwiftfulFirebaseBootcampApp.swift
@@ -5,10 +5,16 @@
 //  Created by Mateusz Brychczynski on 05/09/2023.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct SwiftfulFirebaseBootcampApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+        print("Configured Firebase!")
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
