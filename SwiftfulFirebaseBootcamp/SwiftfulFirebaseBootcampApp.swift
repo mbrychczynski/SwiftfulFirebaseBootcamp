ce799b5600ffeb91a2389d8b15a73c5fc41fc3f5
Mateusz Brychczynski <mat.brych@gmail.com>
2023-09-05 16:50:47 +0100

    Init

---
+++ SwiftfulFirebaseBootcamp/SwiftfulFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulFirebaseBootcampApp.swift
+//  SwiftfulFirebaseBootcamp
+//
+//  Created by Mateusz Brychczynski on 05/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
